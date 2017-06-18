da9b9d188065361e30c677a763b694d640ee4b31
Dave Franco <dfranco@groupon.com>
2017-06-18 13:58:01 -0400

    remove old comments, add multiple valid groups to user and multiple public_keys

--- user.go
+++ user.go
@@ -14,7 +14,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	osuser "os/user"
 	"strings"
 )
 
@@ -22,25 +21,7 @@ type User struct {
 	Name      string   `json:"name"`
 	Sudoer    bool     `json:"sudoer"`
 	Groups    []string `json:"groups"`
-	Publickey string   `json:"public_key"`
-}
-
-//Check if user exist
-func checkUser(username string) bool {
-	_, err := osuser.Lookup(username)
-	if err != nil {
-		return false
-	}
-	return true
-}
-
-//Check if group exist
-func checkGroup(groupname string) bool {
-	_, err := osuser.LookupGroup(groupname)
-	if err != nil {
-		return false
-	}
-	return true
+	Publickey []string `json:"public_key"`
 }
 
 func (usr User) Add() error {
@@ -51,50 +32,56 @@ func (usr User) Add() error {
 		//This means that the user doesn'exist
 		cmd := "useradd"
 
+		//When are more than one group to add
 		if len(usr.Groups) > 0 {
 			//If the length of the field group is greater than zero check groups and then
-			validGroup := make([]string, len(usr.Groups))
+
+			var validGroup []string
 			for _, v := range usr.Groups {
+				//Is an existing group on the system?
 				if checkGroup(v) {
 					validGroup = append(validGroup, v)
 				}
 			}
-
 			//Run useradd plus the groups that the user should be member
 			err := Execute(cmd, "-G", strings.Join(validGroup, ","), "-m", usr.Name)
 			if err != nil {
-				return err //This should go to a log
+				return err
 			}
 
-		}
-		err := Execute(cmd, "-m", usr.Name)
-		if err != nil {
-			return err
+		} else {
+			//add user without groups
+			err := Execute(cmd, "-m", usr.Name)
+			if err != nil {
+				return err
+			}
 		}
 
 		//create ssh directory
 		sshdir := fmt.Sprintf("/home/%s/.ssh", usr.Name)
 		err := os.Mkdir(sshdir, 0644)
 		if err != nil {
-			//fmt.Println("Unable to add ssh directory")
-			//os.Exit(1)
 			return err
 		}
 
-		//Add SSH key, should this be a separate method?
-		aerr := ioutil.WriteFile(sshdir+"/authorized_keys", []byte(usr.Publickey), 0600)
-		if aerr != nil {
-			fmt.Println(aerr)
+		//Add public keys
+		f, err := os.OpenFile(sshdir+"/authorized_keys", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+		if err != nil {
+			panic(err)
+		}
+		for _, v := range usr.Publickey {
+			if _, err = f.WriteString(v + "\n"); err != nil {
+				panic(err)
+			}
 		}
 
 		//Change ownership to the user
-		err := Execute("chown", "-R", string(fmt.Sprintf("%s:%s", usr.Name, usr.Name)), sshdir)
-		//fmt.Println(string(fmt.Sprintf("%s:%s", usr.name)), sshdir)
-
-		fmt.Println("Successfully user created")
-	} else {
-		fmt.Println("user already exist")
+		err = Execute("chown", "-R", string(fmt.Sprintf("%s:%s", usr.Name, usr.Name)), sshdir)
+		if err != nil {
+			return err
+		}
 	}
+	return nil
 }
 
 func (usr User) Del() {
@@ -118,3 +105,28 @@ func (usr User) Makesudo() {
 		panic(err)
 	}
 }
+
+/*
+Me playing around
+func main() {
+	//Add user
+	public_key := []string{
+		`ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDNSloNNYSsk6ULLCbsPh5US47iq0dtYzE8gpUJeJrFDerGCrpMkfJdOtF78VS80lkl0M5p7GS0Sm3E/4tldXmyvo1GM/zJnBw7VuJ1Z7FI2x4hBu6z3eLBw8BWV0edrMKe2EXhHkbkly2fTtD7XKDr53coZ8G8/vtchEYtTnZ18FrAhE9SaWth20eQ47liJjK/sW0U3RmsL+5M2yxPt5LyftxxrAKg+YDyfbhijfvmvwrUVoyPWI3p9ndLhig4BciK5IUaUnDtZIPQmXxYLTdW5fgi+A8AeS66d3uiwWj3Au6yii1xwsQE/6YnzyudEVuBGtjRneTP8Yck81m4sxozZSp6++W0BhEQrWifzilcPLNr62myG2pUtidHlZtd9qEGA4sK3qbzsXF9ku0F0kLtjvdXJeRo6breHhQuvBLGZWDbZuqeM/tryp/75VyAOkI3fX0qTH6VW3Y881Z1ah9AqwcAIeGj1dyvuPGg2v7wxn+7BM5RwDLfqwAXUmIqoCzPvwxV2/uhgHTH4ZDoqyshzaqcKJhf+Py/DESAT3MQl+qnNzc5Uz4YPXj61dwmOUvkffPkzzrNEprm+vFn6AnWUBGWRHuK3k61wqW6BIJuNYn5nyKYJGLOUVVEZMulgSnI4FajOb8AoKs30EAITNMbC5n3T+DMNyOAAZDjQ+gdIQ== groupon@C02NLP3YG3QC.group.on`,
+		`ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDNSloNNYSsk6ULLCbsPh5US47iq0dtYzE8gpUJeJrFDerGCrpMkfJdOtF78VS80lkl0M5p7GS0Sm3E/4tldXmyvo1GM/zJnBw7VuJ1Z7FI2x4hBu6z3eLBw8BWV0edrMKe2EXhHkbkly2fTtD7XKDr53coZ8G8/vtchEYtTnZ18FrAhE9SaWth20eQ47liJjK/sW0U3RmsL+5M2yxPt5LyftxxrAKg+YDyfbhijfvmvwrUVoyPWI3p9ndLhig4BciK5IUaUnDtZIPQmXxYLTdW5fgi+A8AeS66d3uiwWj3Au6yii1xwsQE/6YnzyudEVuBGtjRneTP8Yck81m4sxozZSp6++W0BhEQrWifzilcPLNr62myG2pUtidHlZtd9qEGA4sK3qbzsXF9ku0F0kLtjvdXJeRo6breHhQuvBLGZWDbZuqeM/tryp/75VyAOkI3fX0qTH6VW3Y881Z1ah9AqwcAIeGj1dyvuPGg2v7wxn+7BM5RwDLfqwAXUmIqoCzPvwxV2/uhgHTH4ZDoqyshzaqcKJhf+Py/DESAT3MQl+qnNzc5Uz4YPXj61dwmOUvkffPkzzrNEprm+vFn6AnWUBGWRHuK3k61wqW6BIJuNYn5nyKYJGLOUVVEZMulgSnI4FajOb8AoKs30EAITNMbC5n3T+DMNyOAAZDjQ+gdIQ== groupon@C02NLP3YG3QC.group.com`,
+		`ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDNSloNNYSsk6ULLCbsPh5US47iq0dtYzE8gpUJeJrFDerGCrpMkfJdOtF78VS80lkl0M5p7GS0Sm3E/4tldXmyvo1GM/zJnBw7VuJ1Z7FI2x4hBu6z3eLBw8BWV0edrMKe2EXhHkbkly2fTtD7XKDr53coZ8G8/vtchEYtTnZ18FrAhE9SaWth20eQ47liJjK/sW0U3RmsL+5M2yxPt5LyftxxrAKg+YDyfbhijfvmvwrUVoyPWI3p9ndLhig4BciK5IUaUnDtZIPQmXxYLTdW5fgi+A8AeS66d3uiwWj3Au6yii1xwsQE/6YnzyudEVuBGtjRneTP8Yck81m4sxozZSp6++W0BhEQrWifzilcPLNr62myG2pUtidHlZtd9qEGA4sK3qbzsXF9ku0F0kLtjvdXJeRo6breHhQuvBLGZWDbZuqeM/tryp/75VyAOkI3fX0qTH6VW3Y881Z1ah9AqwcAIeGj1dyvuPGg2v7wxn+7BM5RwDLfqwAXUmIqoCzPvwxV2/uhgHTH4ZDoqyshzaqcKJhf+Py/DESAT3MQl+qnNzc5Uz4YPXj61dwmOUvkffPkzzrNEprm+vFn6AnWUBGWRHuK3k61wqW6BIJuNYn5nyKYJGLOUVVEZMulgSnI4FajOb8AoKs30EAITNMbC5n3T+DMNyOAAZDjQ+gdIQ== groupon@C02NLP3YG3QC.group.cl`,
+	}
+	user := User{
+		Name:      "dfranco",
+		Sudoer:    true,
+		Groups:    []string{"adm", "operator"},
+		Publickey: public_key,
+	}
+
+	//Add user
+	err := user.Add()
+	if err != nil {
+		fmt.Printf("Error is %s", err)
+	}
+
+}
+*/
