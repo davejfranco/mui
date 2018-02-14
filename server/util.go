7817ee3f7e92e50c4659096dc133b877064a39ed
Dave <dfranco@grouponlatam.com>
2018-02-13 22:06:00 -0300

    string to Null converter function

--- server/util.go
+++ server/util.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"time"
 )
 
 func CurrentTime() string {
 	t := time.Now()
 	return t.Format("2006-01-02 15:04:05")
+}
+
+func NewNullString(s string) sql.NullString {
+	if len(s) == 0 {
+		return sql.NullString{}
+	}
+	return sql.NullString{
+		String: s,
+		Valid:  true,
+	}
 }
