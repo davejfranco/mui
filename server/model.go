12d70a1b8dedebd56464a1013cda24835140cff1
Dave <dfranco@grouponlatam.com>
2018-02-13 22:03:50 -0300

    User Model and methods ready.. needs more tests

--- server/model.go
+++ server/model.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -64,33 +65,24 @@ func (db MysqlConfig) Conn() *sql.DB {
 //User type
 type User struct {
 	Username  string `db:"username" json:"username"`
+	Sudo      string `db:"sudo" json:"sudo"`
 	FullName  string `db:"full_name" json:"full_name"`
+	PublicKey string `db:"public_key" json:"public_key"`
 	Created   string `db:"created" json:"created"`
-	PublicKey string `db:"public_key" json:"publickey"`
 }
 
-type Group struct {
-	GroupId string `db:"groupid"`
-	Users   []User
+type UserModel struct {
+	*User
+	Conn *sql.DB
 }
 
-//Server type
-type Server struct {
-	Ec2Id    string `db:"ec2_id" json:"ec2_id"`
-	ServerIP string `db:"server_ip" json:"server_ip"`
-}
+//Check if user exist
+func (u UserModel) Exist() bool {
 
-type Model struct {
-	MysqlConfig
-}
-
-//IfExist user
-func (user *User) ifExist(conn *sql.DB) bool {
-
-	query := "SELECT username FROM mui.users WHERE username=?"
+	query := "SELECT username FROM users WHERE username=?"
 	var username string
 
-	err := conn.QueryRow(query, user.Username).Scan(&username)
+	err := u.Conn.QueryRow(query, u.Username).Scan(&username)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return false
@@ -100,48 +92,114 @@ func (user *User) ifExist(conn *sql.DB) bool {
 	return true
 }
 
-//Add new user
-func (user *User) newUser(conn *sql.DB) error {
+//Get specific user
+func (u UserModel) Get() (User, error) {
+	query := "SELECT full_name, sudo,  public_key, created FROM users WHERE username=?"
 
-	if user.ifExist(conn) {
+	var (
+		fullname  sql.NullString //This field can be Null
+		sudo      string
+		publickey sql.NullString //This field can also be Null
+		created   string
+	)
+
+	err := u.Conn.QueryRow(query, u.Username).Scan(&fullname,
+		&sudo,
+		&publickey,
+		&created)
+
+	if err != nil {
+		return User{}, err
+	}
+	user := User{
+		Username: u.Username,
+		Sudo:     sudo,
+		Created:  created,
+	}
+
+	if fullname.Valid {
+		user.FullName = fullname.String
+	}
+
+	if publickey.Valid {
+		user.PublicKey = publickey.String
+	}
+
+	return user, nil
+}
+
+//Add new user
+func (u *UserModel) New() error {
+
+	if u.Exist() {
 		return nil
 	}
 
-	insertquery := "INSERT INTO mui.users (username, full_name, created) VALUES (?, ?, ?)"
-	//not sure if to set the current time in here
-	user.Created = CurrentTime()
-	_, err := conn.Exec(insertquery, user.Username, user.FullName, user.Created)
+	switch {
+	case len(u.Username) == 0:
+		return errors.New("Username field is required")
+	case len(u.Sudo) == 0:
+		return errors.New("Sudo field is required")
+	}
+
+	insertquery := "INSERT INTO users (username, full_name, sudo, public_key, created) VALUES (?, ?, ?, ?, ?)"
+	_, err := u.Conn.Exec(insertquery, u.Username, NewNullString(u.FullName), u.Sudo, NewNullString(u.PublicKey), CurrentTime())
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-//modUser
-func (user *User) modUser(conn *sql.DB) error {
+//Update user
+func (u *UserModel) Update(field, value string) error {
 	//"UPDATE mui.users SET full_name="Carlos D. Rodriguez" WHERE username='crodriguez'"
-	modquery := "UPDATE mui.users SET username=?, full_name=? WHERE username=?"
-	_, err := conn.Exec(modquery, user.Username, user.FullName, user.Username)
+	if field != "username" && field != "full_name" && field != "sudo" && field != "public_key" {
+		return errors.New("Invalid field")
+	}
+
+	queryrun := func(q, v string) error {
+		_, err := u.Conn.Exec(q, v, u.Username)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	switch {
+	case field == "username":
+		query := fmt.Sprintf("Update users SET username=? WHERE username=?")
+		u.Username = value
+		return queryrun(query, value)
+	case field == "full_name":
+		query := fmt.Sprintf("Update users SET full_name=? WHERE username=?")
+		u.FullName = value
+		return queryrun(query, value)
+	case field == "sudo":
+		query := fmt.Sprintf("Update users SET sudo=? WHERE username=?")
+		u.Sudo = value
+		return queryrun(query, value)
+	case field == "public_key":
+		query := fmt.Sprintf("Update users SET public_key=? WHERE username=?")
+		u.PublicKey = value
+		return queryrun(query, value)
+	}
+	return nil
+}
+
+//Delete user
+func (u *UserModel) Del() error {
+	delquery := "DELETE FROM users WHERE username=?"
+	_, err := u.Conn.Exec(delquery, u.Username)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-//delUser hate this comments
-func (user *User) delUser(conn *sql.DB) error {
-	delquery := "DELETE FROM mui.users WHERE username=?"
-	_, err := conn.Exec(delquery, user.Username)
-	if err != nil {
-		return err
-	}
-	return nil
-}
+//Get all users
+func (u UserModel) Getall() []User {
 
-func getAllUsers(conn *sql.DB) []User {
-
-	query := "SELECT * FROM mui.users"
-	rows, err := conn.Query(query)
+	rows, err := u.Conn.Query("SELECT * FROM users")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -149,24 +207,49 @@ func getAllUsers(conn *sql.DB) []User {
 	var allusers []User
 	for rows.Next() {
 		var (
-			username string
-			fullname string
-			created  string
+			username  string
+			fullname  sql.NullString //This field can be Null
+			created   string
+			publickey sql.NullString //This field can also be Null
 		)
 
-		err = rows.Scan(&username, &fullname, &created)
+		err = rows.Scan(&username, &fullname, &created, &publickey)
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		user := User{
 			Username: username,
-			FullName: fullname,
 			Created:  created,
 		}
+
+		if fullname.Valid {
+			user.FullName = fullname.String
+		}
+
+		if publickey.Valid {
+			user.PublicKey = publickey.String
+		}
+
 		allusers = append(allusers, user)
 	}
 	return allusers
+}
 
+type Group struct {
+	GroupId string `db:"groupid"`
+	Users   []User
+}
+
+type GroupModel struct {
+	Group
+	Conn *sql.DB
+}
+
+//Server type
+type Server struct {
+	Ec2Id    string `db:"ec2_id" json:"ec2_id"`
+	ServerIP string `db:"server_ip" json:"server_ip"`
 }
 
 //Add node
